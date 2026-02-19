89bdc17b2e4a3f99132b59964539f9dd6c340424
ARTHUR PEREIRA GUSMÃO <arthur.gusmao@portalsesisp.org.br>
2026-02-19 11:33:36 -0300

    Entregando Ex1, 2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,13 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    //private static string nome = "Arthur Gusmao";
+    
+    public static void Executar()
+    {
+        string nome = "Arthur Gusmao";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+
+}
